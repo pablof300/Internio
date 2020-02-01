ee9c78e8a625f6b3bdbb4fbf9df3f5bbaa2669e4
Legacy35 <aarongonzalez@ufl.edu>
2020-02-01 02:29:12 -0500

    Fixed and completed all 4 models

--- service/src/main/java/me/pabloestrada/core/user/UserPreferences.java
+++ service/src/main/java/me/pabloestrada/core/user/UserPreferences.java
@@ -1,50 +1,23 @@
 package me.pabloestrada.core.user;
 
-import org.bson.types.ObjectId;
-
-import java.util.Objects;
-
 public final class UserPreferences
 {
-    private String username;
-    private String password;
-    private ObjectId id;
-
+    private int [] questionResponses = new int[10];
     public UserPreferences() {
     }
 
-    public UserPreferences(String username, String password) {
-        this.username = username;
-        this.password = password;
+    public UserPreferences(int [] questionResponses) {
+        this.questionResponses = questionResponses;
     }
 
-    public UserPreferences(String username, String password, boolean generateId) {
-        this.username = username;
-        this.password = password;
-        this.id = new ObjectId();
+    public int[] getQuestionResponses() {
+        return questionResponses;
     }
 
-    public String getUsername() {
-        return username;
+    public void setQuestionResponses(int[] questionResponses) {
+        this.questionResponses = questionResponses;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
 }
--- service/src/main/java/me/pabloestrada/core/user/UserSettings.java
+++ service/src/main/java/me/pabloestrada/core/user/UserSettings.java
@@ -1,50 +1,12 @@
 package me.pabloestrada.core.user;
 
-import org.bson.types.ObjectId;
-
-import java.util.Objects;
-
 public final class UserSettings
 {
-    private String username;
-    private String password;
-    private ObjectId id;
+   //Empty for now until we can think of settings to actually add
 
     public UserSettings() {
+
     }
 
-    public UserSettings(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 
-    public UserSettings(String username, String password, boolean generateId) {
-        this.username = username;
-        this.password = password;
-        this.id = new ObjectId();
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
 }
